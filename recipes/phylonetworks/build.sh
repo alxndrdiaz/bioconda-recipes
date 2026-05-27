d68656690c6ae5e1ddcb6094fac1c984a0887e39
alxndrdiaz <ra.ramos.diaz@gmail.com>
2026-05-26 20:33:57 -0600

    attempt to copy scripts

--- recipes/phylonetworks/build.sh
+++ recipes/phylonetworks/build.sh
@@ -2,3 +2,8 @@
 
 # install phylonetworks
 julia -e 'using Pkg; Pkg.add(name="PhyloNetworks", version="1.3.1")'
+
+# copy the scripts and dependencies
+PHYLONETWORKS_DIR=${PREFIX}/share/phylonetworks/src
+mkdir -p "${PHYLONETWORKS_DIR}"
+cp -r {src,Project.toml} "${PHYLONETWORKS_DIR}"
