7671a1cd06bb47d362911ce73c51a0271cf3ce2e
alxndrdiaz <ra.ramos.diaz@gmail.com>
2026-05-27 18:01:54 -0600

    use share dir

--- recipes/phylonetworks/build.sh
+++ recipes/phylonetworks/build.sh
@@ -4,6 +4,9 @@
 julia -e 'using Pkg; Pkg.add(name="PhyloNetworks", version="1.3.1")'
 
 # copy the scripts and dependencies
-PHYLONETWORKS_DIR=${PREFIX}/share/phylonetworks/src
+PHYLONETWORKS_DIR=${PREFIX}/share/phylonetworks
 mkdir -p "${PHYLONETWORKS_DIR}"
-cp -r {src,Project.toml} "${PHYLONETWORKS_DIR}"
+cp ./Project.toml "${PHYLONETWORKS_DIR}"
+cp -r ./src "${PHYLONETWORKS_DIR}"
+mkdir -p "${PREFIX}/bin"
+ln -s "${PHYLONETWORKS_DIR}/src/PhyloNetworks.jl" "${PREFIX}/bin/PhyloNetworks.jl"
