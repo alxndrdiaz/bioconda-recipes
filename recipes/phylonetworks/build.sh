d3c5aa1a3839b4806a804a2826c6ca69e53638a9
alxndrdiaz <ra.ramos.diaz@gmail.com>
2026-05-28 22:02:29 -0600

    try to use shared dir

--- recipes/phylonetworks/build.sh
+++ recipes/phylonetworks/build.sh
@@ -4,9 +4,7 @@
 julia -e 'using Pkg; Pkg.add(name="PhyloNetworks", version="1.3.1")'
 
 # copy the scripts and dependencies
-PHYLONETWORKS_DIR=${PREFIX}/share/phylonetworks
+PHYLONETWORKS_DIR=$PREFIX/share/julia
 mkdir -p "${PHYLONETWORKS_DIR}"
 cp ./Project.toml "${PHYLONETWORKS_DIR}"
 cp -r ./src "${PHYLONETWORKS_DIR}"
-mkdir -p "${PREFIX}/bin"
-ln -s "${PHYLONETWORKS_DIR}/src/PhyloNetworks.jl" "${PREFIX}/bin/PhyloNetworks.jl"
